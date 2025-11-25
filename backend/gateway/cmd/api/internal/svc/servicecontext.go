dff1233ceb0f1bac15d7f988ac6bc8c07f42892f
liuzao <lz790929231@163.com>
2025-11-25 20:13:28 +0800

    feat: 添加网关调用rpc

--- backend/gateway/cmd/api/internal/svc/servicecontext.go
+++ backend/gateway/cmd/api/internal/svc/servicecontext.go
@@ -5,14 +5,22 @@ package svc
 
 import (
 	"jike_todo/gateway/cmd/api/internal/config"
+	"jike_todo/task/cmd/rpc/taskservice"
+	"jike_todo/user/cmd/rpc/authservice"
+
+	"github.com/zeromicro/go-zero/zrpc"
 )
 
 type ServiceContext struct {
-	Config config.Config
+	Config  config.Config
+	UserRpc authservice.AuthService
+	TaskRpc taskservice.TaskService
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
+		Config:  c,
+		UserRpc: authservice.NewAuthService(zrpc.MustNewClient(c.UserRpcClient)),
+		TaskRpc: taskservice.NewTaskService(zrpc.MustNewClient(c.TaskRpcClient)),
 	}
 }
