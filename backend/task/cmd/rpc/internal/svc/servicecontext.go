9acddbf352d3b2500a18d012bd8d2cf1ea642553
liuzao <lz790929231@163.com>
2025-11-25 20:22:50 +0800

    feat: 添加rpc的上下文

--- backend/task/cmd/rpc/internal/svc/servicecontext.go
+++ backend/task/cmd/rpc/internal/svc/servicecontext.go
@@ -1,13 +1,22 @@
 package svc
 
-import "jike_todo/task/cmd/rpc/internal/config"
+import (
+	"jike_todo/task/cmd/rpc/internal/config"
+	"jike_todo/task/model"
+
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
 
 type ServiceContext struct {
-	Config config.Config
+	Config    config.Config
+	TaskModel model.TasksModel
+	CateModel model.CategoriesModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
+		Config:    c,
+		TaskModel: model.NewTasksModel(sqlx.NewMysql(c.DataSource), c.Cache),
+		CateModel: model.NewCategoriesModel(sqlx.NewMysql(c.DataSource), c.Cache),
 	}
 }
--- backend/user/cmd/rpc/internal/svc/servicecontext.go
+++ backend/user/cmd/rpc/internal/svc/servicecontext.go
@@ -1,13 +1,21 @@
 package svc
 
-import "jike_todo/user/cmd/rpc/internal/config"
+import (
+	"jike_todo/user/cmd/rpc/internal/config"
+
+	"jike_todo/user/model"
+
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
 
 type ServiceContext struct {
-	Config config.Config
+	Config    config.Config
+	UserModel model.UsersModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
+		Config:    c,
+		UserModel: model.NewUsersModel(sqlx.NewMysql(c.DataSource), c.Cache),
 	}
 }
